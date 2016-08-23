c93057254c890caff72fd9d850dc73ca4b4fbbca
waston <719254212@qq.com>
2016-08-23 11:42:26 +0800

    提交工作文档
    同上...

---
+++ GitExperience/Assets/Script/ShowView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowView : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("!1");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
